9ad078792a28acd5d369422848c9c52b667d6155
hahaha <a310608318@gmail.com>
2025-02-12 11:10:58 +0800

    2.12

---
+++ hello.asm
@@ -0,0 +1,16 @@
+section .data
+   msg db "Hello,world!", 0
+   len equ $ - msg
+
+section .text
+   global _start
+_start:
+   mov rax, 1
+   mov rdi, 1
+   mov rsi, msg
+   mov rdx, len
+   syscall
+   
+   mov rax, 60
+   xor rdi,rdi
+   syscall
